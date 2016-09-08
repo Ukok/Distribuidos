ea8daca766409f40ff91a3407d11f53478a992ca
mario <mario@debian>
2016-09-08 17:01:27 -0500

    Test

---
+++ Distribui2_Ejercicio03_Lamport/src/com/ejercicio03/Main_02XD.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ejercicio03;
+
+/**
+ *
+ * @author mario
+ */
+public class Main_02XD {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo...");
+    }
+}
